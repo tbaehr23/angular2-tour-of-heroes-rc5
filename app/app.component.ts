cdaf997fd846ec83c5481035912bfa6ed55e28b9
tbaehr23 <tbaehr@solnwerks.com>
2016-08-20 17:01:02 -0500

    3.a Implement master/detail with selectable list of heroes
    move template to separate .html file
    
    add static array of Hero and assign to heroes property
    change hero property to selectedHero and remove assignment
    show list of heroes using <ul> and <li *ngFor="">
    bind <li> click event to onSelect(hero) method that assigns parameter to
    selectedHero property
    
    wrap simple editor in <div *ngIf> to prevent error from binding to
    undefined selectedHero property

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,25 +6,31 @@ export class Hero {
 }
 
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'my-app',
-    template:`
-        <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-    `
+    templateUrl: './app/app.component.html' //relative to index.html
 })
 export class AppComponent {
     
     title = 'Tour of Heroes';
-    
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    onSelect(selection: Hero): void {
+        this.selectedHero = selection;
+    }
 
 }
